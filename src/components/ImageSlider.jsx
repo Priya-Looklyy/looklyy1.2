a7be23c3a45aaba063083d87f878e68669bf02db
Priya-Looklyy <priya.k.stephen@gmail.com>
2025-10-16 20:15:42 +0530

    FIX: Replace pushpin icon with proper bookmark icon SVG path

--- src/components/ImageSlider.jsx
+++ src/components/ImageSlider.jsx
@@ -87,7 +87,7 @@ const ImageSlider = ({ slider, onPinLook }) => {
               aria-label={isPinned(slider.id) ? 'Unpin look' : 'Pin look'}
             >
               <svg viewBox="0 0 24 24" className={`bookmark-icon ${isPinned(slider.id) ? 'icon-filled' : 'icon-outline'}`}>
-                <path d="M17 3H7c-1.1 0-1.99.9-1.99 2L5 21l7-3 7 3V5c0-1.1-.9-2-2-2z"/>
+                <path d="M19 21l-7-5-7 5V5a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2v16z"/>
               </svg>
             </div>
           </div>
